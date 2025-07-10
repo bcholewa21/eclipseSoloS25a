2589ff020819be57e2a8b07bccf038a602c80252
bryson <bcholewa@emich.edu>
2025-07-09 22:35:40 -0400

    wham

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 		System.out.println("Howdy Github Project");
 		System.out.println("Howdy Github Project");
 		System.out.println("Programmer 2 did this");
-		System.out.println("Programer one did this wham");
+		System.out.println("Programer did this wham");
 	}
 
 }
